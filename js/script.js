74cedb60024e178037fb007bbf379f633ff5f459
chrisawesomeusername <chrisawesomeusername@gmail.com>
2019-09-20 11:09:43 +1200

    confirm name closes box

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 console.log('hello');
 
 $(document).ready(function() {
-
+//home page drop down
   $('.burger').click(function() {
   $('.drop-menu').toggle()
 });
 
-  $('.add-btn').click(function(){
-    $('.add-new-toggle').toggle()
-  });
+$('.add-btn').click(function(){
+  $('.add-new-toggle').toggle()
+});
 
+$('.confirm-btn').click(function(){
+  $('.add-new-toggle').toggle()
+});
 
 });
