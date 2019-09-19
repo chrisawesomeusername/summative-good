4cf90eb1b461776f30b474ec7f97e677ed7170dc
chrisawesomeusername <chrisawesomeusername@gmail.com>
2019-09-19 15:37:22 +1200

    js working

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+$(document).ready(function() {
+
+  $('.burger').click(function() {
+  $('.drop-menu').toggle()
+});
+
+  $('.add-btn').click(function(){
+    $('.add-new-toggle').toggle()
+  });
+
+
+});
